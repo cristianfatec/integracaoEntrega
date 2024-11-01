7f85fcde1817514c722438fced38936e48e531eb
cristianfatec <cristian.nascimento@fatec.sp.gov.br>
2024-10-31 22:10:07 -0300

    Teste 3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Teste de processo Nº 2 !');
+  res.end('Teste de processo Nº 3 !');
 });
 
 const PORT = 3000;
