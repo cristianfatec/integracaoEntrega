80c405f19ab6a73a1c26fb3f4f480bd4383f02e5
cristianfatec <cristian.nascimento@fatec.sp.gov.br>
2024-10-31 21:19:57 -0300

    Criação arquivo docker

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Olá, Mundo!');
+  res.end('Hello, world!');
 });
 
 const PORT = 3000;
