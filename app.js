e3af75567b9f6a86618ac92e4c4558b0104954d3
cristianfatec <cristian.nascimento@fatec.sp.gov.br>
2024-10-31 21:38:52 -0300

    Teste de processo de integração 1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, world!');
+  res.end('Teste de processo!');
 });
 
 const PORT = 3000;
